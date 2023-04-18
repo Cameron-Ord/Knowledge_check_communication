6c1418ff75d6ff697d91f9ef43d446fbb03f376d
NuckenMcFuggets <cameron.ian.ronald.ord@Outlook.com>
2023-04-18 17:56:21 -0600

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,96 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="omega_parent">
+    <header class="page_header">
+
+
+    </header>
+    <main class="page_main">
+      <section>
+        <article>
+          <span v-for="user in users" :key="user" :post="user" class="user_profile">
+
+            <h1>{{ user[`name`] }}</h1>
+
+            <p>{{ user[`age`] }}</p>
+
+            <p>{{ user[`is_premium`] }}</p>
+
+            <p>{{ user[`id`] }}</p>
+
+          </span>
+        </article>
+        <article>
+
+          <small-user-profile></small-user-profile>
+
+        </article>
+      </section>
+    </main>
+    <footer class="page_footer">
+
+
+    </footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import SmallUserProfile from './components/SmallUserProfile.vue';
 
 export default {
+
+
+  data() {
+    return {
+      users: [{
+
+
+
+        name: `Cameron`,
+
+        age: `25`,
+
+        is_premium: true,
+
+        id: 327623
+
+      },
+
+
+      {
+
+
+        name: `James`,
+
+        age: `29`,
+
+        is_premium: true,
+
+        id: 219823
+
+
+
+      },
+
+      ]
+    }
+  },
+
+
+
+
+
   name: 'App',
   components: {
-    HelloWorld
-  }
+
+    SmallUserProfile
+
+  },
+
+
+
+
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ src/components/SmallUserProfile.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="parent">
+
+        <span class="profile_span">
+
+            <h1>{{ user[`name`] }}</h1>
+
+            <p> {{ user[`is_premium`] }}</p>
+
+        </span>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        
+
+        props: {
+
+
+            post: Object
+
+
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
